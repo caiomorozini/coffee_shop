c180a26ac9da3c43bc4eae07d5cf2656b0155119
mariannabaldez <mariannabaldez@gmail.com>
2023-10-02 19:44:52 -0300

    constroi tabela de produtos e pedidos

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 ﻿import sqlalchemy
+from sqlalchemy.dialects.postgresql import JSON
 import databases
 
 DATABASE_URL = "postgresql://usuario:senha@localhost:5432/coffeeshop_dev"
@@ -44,6 +45,69 @@ ingredients = sqlalchemy.Table(
         "batch_id",
         sqlalchemy.Integer,
         sqlalchemy.ForeignKey("batches.id")
+    ),
+    sqlalchemy.Column(
+        "observations",
+        sqlalchemy.String(500)
+    ),
+    sqlalchemy.Column(
+        "created_at",
+        sqlalchemy.TIMESTAMP(timezone=True),
+        server_default=sqlalchemy.func.now(),
+    ),
+    sqlalchemy.Column(
+        "updated_at",
+        sqlalchemy.TIMESTAMP(timezone=True),
+        server_default=sqlalchemy.func.now(),
+        onupdate=sqlalchemy.func.now(),
+    ),
+)
+
+products = sqlalchemy.Table(
+    "products",
+    metadata,
+    sqlalchemy.Column(
+        "id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column(
+        "name", sqlalchemy.String(50)),
+    sqlalchemy.Column(
+        "price", sqlalchemy.Float),
+    sqlalchemy.Column(
+        "ingredients",
+        sqlalchemy.Integer,
+        sqlalchemy.ForeignKey("ingredients.id")
+    ),
+    sqlalchemy.Column(
+        "descript",
+        sqlalchemy.String(500)
+    ),
+    sqlalchemy.Column(
+        "created_at",
+        sqlalchemy.TIMESTAMP(timezone=True),
+        server_default=sqlalchemy.func.now(),
+    ),
+    sqlalchemy.Column(
+        "updated_at",
+        sqlalchemy.TIMESTAMP(timezone=True),
+        server_default=sqlalchemy.func.now(),
+        onupdate=sqlalchemy.func.now(),
+    ),
+)
+
+orders = sqlalchemy.Table(
+    "orders",
+    metadata,
+    sqlalchemy.Column(
+        "id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column(
+        "products", sqlalchemy.ARRAY(
+            sqlalchemy.Integer,
+            sqlalchemy.ForeignKey("products.id")
+        )
+    ),
+    sqlalchemy.Column(
+        "price",
+        sqlalchemy.Float,
     ),
     sqlalchemy.Column(
         "observations",
