7c3543a090ceb854952cbe9cac51418a2442864a
caiomorozini <caiomorozini@outlook.com.br>
2023-10-02 21:57:01 -0300

    adiciona rotas para tabela lote

---
+++ app/routers/batches.py
@@ -0,0 +1,103 @@
+﻿from fastapi import (
+    Query,
+    Path, Body, Header, Response, status, Form, File,
+    UploadFile, HTTPException,
+    APIRouter
+)
+from typing import Union, List, Annotated
+from app.schemas.batch import Batch, BatchResponse
+from app.database.database import database, batches
+
+
+
+route = APIRouter()
+
+@route.post("/")
+async def create_item(
+    batch: Batch = Body(
+        ...,
+        **Batch.model_config,
+        openapi_examples={
+            "normal": {
+                "summary": "Um exemplo normal",
+                "description": "Um exemplo normal",
+                "value": {
+                    "purchase": "01/01/2021",
+                    "expiration": "01/01/2022",
+                    "manufacturing": "01/01/2021",
+                }
+            },
+            "invalid": {
+                "summary": "Um exemplo inválido",
+                "description": "Um exemplo inválido",
+                "value": {
+                    "purchase": "01/01/2021",
+                    "expiration": "01/01/2020",
+                    "manufacturing": "01/01/2021",
+                },
+            },
+            "isoformat": {
+                "summary": "Um exemplo inválido",
+                "description": "Um exemplo inválido",
+                "value": {
+                    "purchase": "2021-01-01T00:00:00",
+                    "expiration": "2020-01-01T00:00:00",
+                    "manufacturing": "2021-01-01T00:00:00",
+                },
+            },
+        }
+    )) -> BatchResponse:
+
+    # Checando se o lote já existe
+    query = batches.select().where(
+        (batches.c.purchase == batch.purchase) &
+        (batches.c.expiration == batch.expiration) &
+        (batches.c.manufacturing == batch.manufacturing)
+    )
+    batch_exists = await database.fetch_one(query)
+
+    if batch_exists:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Lote já existe"
+        )
+
+    # Cria comando SQL para inserir o lote e executa
+    query = batches.insert().values(**batch.model_dump())
+    last_record_id = await database.execute(query)
+
+    return BatchResponse(id=last_record_id, **batch.model_dump())
+
+
+@route.get(
+    "/",
+    # tags=[Tags.items, Tags.users],
+    summary="Mostra os cafés registrados",
+    response_description="A lista de cafés registrados",
+    response_model=List[BatchResponse],
+)
+async def mostrar_items(
+    query: list = Query(
+        default_factory=list,
+        title="Query string",
+        description="Query string para filtrar os items",
+        alias="abc",
+        ),
+    limit: int = Query(default=10, ge=1, le=50),
+    recent: bool = False
+):
+    """
+    Create an item with all the information:
+
+    - **name**: each item must have a name
+    - **description**: a long description
+    - **price**: required
+    - **tax**: if the item doesn't have tax, you can omit this
+    - **tags**: a set of unique tag strings for this item
+    """
+
+    query = batches.select().limit(limit)
+    if recent is True:
+        query = batches.select().order_by(batches.c.id.desc()).limit(limit)
+
+    return await database.fetch_all(query)
---
+++ app/schemas/batch.py
@@ -0,0 +1,52 @@
+﻿#pylint: disable=E0213
+
+from pydantic import BaseModel, confloat, constr, Field, HttpUrl, validator
+from datetime import datetime
+from typing import Union, List, Annotated
+
+
+class Batch(BaseModel):
+    """ Modelo de lote """
+    purchase: datetime
+    expiration: datetime
+    manufacturing: datetime
+
+    @validator("purchase", "expiration", "manufacturing", pre=True)
+    def format_date(cls, v):
+        """ Formata a data para datetime """
+
+        if isinstance(v, str):
+            try:
+                v = datetime.strptime(v, "%d/%m/%Y")
+            except ValueError:
+                try:
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S")
+                except ValueError as value_error:
+                    raise ValueError(
+                        f"Data {v} não está no formato dd/mm/yyyy") \
+                        from value_error
+        return v
+
+    @validator("purchase", "manufacturing")
+    def purchase_and_manufacturing_must_be_before_today(cls, v):
+        """ Verifica se a data é anterior a hoje """
+        if v is not None:
+            if v > datetime.now():
+                raise ValueError(f"Data {v} é posterior a hoje")
+            if v < datetime(year=2000, month=1, day=1):
+                raise ValueError(f"Data {v} é anterior a data mínima permitida")
+        return v
+
+    @validator("expiration")
+    def expiration_must_be_after_purchase(cls, v, values, **kwargs):
+        """ Verifica se a data é posterior a data de compra """
+        if not values.get("purchase"):
+            raise ValueError("Data de compra não foi informada")
+        if v is not None:
+            if v < values["purchase"]:
+                raise ValueError("Data de expiração é anterior a data de compra")
+        return v
+
+class BatchResponse(Batch):
+    """ Modelo de resposta de lote """
+    id: int
