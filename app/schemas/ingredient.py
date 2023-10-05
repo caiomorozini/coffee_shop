62b27893abc4b4b6d117eda1795c06f5e49acddb
caiomorozini <caiomorozini@outlook.com.br>
2023-10-04 22:56:01 -0300

    coloca observação como opcional

--- app/schemas/ingredient.py
+++ app/schemas/ingredient.py
@@ -1,9 +1,10 @@
 ﻿#pylint: disable=E0213
 
+import re
 from pydantic import BaseModel, confloat, constr, Field, HttpUrl, validator
 from datetime import datetime
 from typing import Union, List, Annotated, Optional
-
+from unidecode import unidecode
 
 class Ingredient(BaseModel):
     """ Modelo de ingrediente """
@@ -13,12 +14,16 @@ class Ingredient(BaseModel):
         to_lower=True,
         min_length=1,
         ) = Field(..., example="tomate")
-    quantity: confloat(gt=0) = Field(..., example=1)
-    observations: constr(
+    observations: Optional[constr(
         max_length=500,
         strip_whitespace=True,
         min_length=1,
-        ) = Optional[Field(..., example="tomate")]
+     )] = Field(None, example="comprado no mercado")
+
+    @validator("name")
+    def apply_unidecode(cls, v):
+        """ Aplica unidecode no nome """
+        return re.sub(r'[^\w\s]', '', unidecode(v))
 
 class IngredientResponse(Ingredient):
     """ Modelo de resposta de ingrediente """
